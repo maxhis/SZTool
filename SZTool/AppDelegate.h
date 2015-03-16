3a8064ee26a006ba480dde0271d36123302cb969
振理 <aixing.max@alibaba-inc.com>
2015-03-16 11:26:21 +0800

    Initial Commit

---
+++ SZTool/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SZTool
+//
+//  Created by iStar on 15/3/16.
+//  Copyright (c) 2015年 Code Addict Studio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SZTool/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SZTool
+//
+//  Created by iStar on 15/3/16.
+//  Copyright (c) 2015年 Code Addict Studio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+

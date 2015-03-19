73422d2cc8ac2d19c58e6300ea223e8acf56f2a4
iStar <istar309@gmail.com>
2015-03-19 12:29:07 +0800

    新的首页菜单

--- SZTool/Home/SZTHomeController.m
+++ SZTool/Home/SZTHomeController.m
@@ -7,20 +7,21 @@
 //
 
 #import "SZTHomeController.h"
-#import "CNPGridMenu.h"
 #import "SZTGongjijinController.h"
 #import "SZTYaohaoViewController.h"
 #import "SZTShebaoViewController.h"
 #import "RZTransitionsManager.h"
+#import "AwesomeMenu.h"
 
-@interface SZTHomeController () <CNPGridMenuDelegate>
+@interface SZTHomeController () <AwesomeMenuDelegate>
 
-@property (nonatomic, strong) CNPGridMenu *gridMenu;
+@property (nonatomic, strong) AwesomeMenu *mainMenu;
 
-@property (nonatomic, strong) CNPGridMenuItem *gongjijinItem;
-@property (nonatomic, strong) CNPGridMenuItem *shebaoItem;
-@property (nonatomic, strong) CNPGridMenuItem *yaohaoItem;
-@property (nonatomic, strong) CNPGridMenuItem *settingsItem;
+@property (nonatomic, strong) AwesomeMenuItem *startItem;
+@property (nonatomic, strong) AwesomeMenuItem *gongjijinItem;
+@property (nonatomic, strong) AwesomeMenuItem *shebaoItem;
+@property (nonatomic, strong) AwesomeMenuItem *yaohaoItem;
+@property (nonatomic, strong) AwesomeMenuItem *settingsItem;
 
 @end
 
@@ -39,61 +40,52 @@
     bgView.image = [UIImage imageNamed:@"background.jpg"];
     [self.view addSubview:bgView];
     
-    UIButton *menuButton = [[UIButton alloc] initWithFrame:CGRectMake(DTScreenWidth/2 - 50, DTScreenHeight/2, 100, 50)];
-    [menuButton addTarget:self action:@selector(showGridMenu:) forControlEvents:UIControlEventTouchUpInside];
-    menuButton.layer.cornerRadius = 4;
-    menuButton.backgroundColor = [UIColor dt_colorWithHexString:@"007cd8"];
-    [menuButton setTitle:@"开始" forState:UIControlStateNormal];
-    [self.view addSubview:menuButton];
-    
     [self initMenu];
 }
 
 - (void)initMenu
 {
-    _gongjijinItem = [[CNPGridMenuItem alloc] init];
-    _gongjijinItem.icon = [UIImage imageNamed:@"gongjijin"];
-    _gongjijinItem.title = @"公积金";
+    UIImage *storyMenuItemImage = [UIImage imageNamed:@"bg-menuitem.png"];
+    UIImage *storyMenuItemImagePressed = [UIImage imageNamed:@"bg-menuitem-highlighted.png"];
+    UIImage *starImage = [UIImage imageNamed:@"icon-star.png"];
+    _gongjijinItem = [[AwesomeMenuItem alloc] initWithImage:storyMenuItemImage
+                                                           highlightedImage:storyMenuItemImagePressed
+                                                               ContentImage:starImage
+                                                    highlightedContentImage:nil];
+    _shebaoItem = [[AwesomeMenuItem alloc] initWithImage:storyMenuItemImage
+                                                           highlightedImage:storyMenuItemImagePressed
+                                                               ContentImage:starImage
+                                                    highlightedContentImage:nil];
+    _yaohaoItem = [[AwesomeMenuItem alloc] initWithImage:storyMenuItemImage
+                                           highlightedImage:storyMenuItemImagePressed
+                                               ContentImage:starImage
+                                    highlightedContentImage:nil];
+    _settingsItem = [[AwesomeMenuItem alloc] initWithImage:storyMenuItemImage
+                                        highlightedImage:storyMenuItemImagePressed
+                                            ContentImage:starImage
+                                 highlightedContentImage:nil];
     
-    _shebaoItem = [[CNPGridMenuItem alloc] init];
-    _shebaoItem.icon = [UIImage imageNamed:@"shebao"];
-    _shebaoItem.title = @"社保";
-    
-    _yaohaoItem = [[CNPGridMenuItem alloc] init];
-    _yaohaoItem.icon = [UIImage imageNamed:@"yaohao"];
-    _yaohaoItem.title = @"摇号";
-    
-    _settingsItem = [[CNPGridMenuItem alloc] init];
-    _settingsItem.icon = [UIImage imageNamed:@"settings"];
-    _settingsItem.title = @"设置";
+    // the start item, similar to "add" button of Path
+    _startItem = [[AwesomeMenuItem alloc] initWithImage:[UIImage imageNamed:@"bg-addbutton.png"]
+                                                       highlightedImage:[UIImage imageNamed:@"bg-addbutton-highlighted.png"]
+                                                           ContentImage:[UIImage imageNamed:@"icon-plus.png"]
+                                                highlightedContentImage:[UIImage imageNamed:@"icon-plus-highlighted.png"]];
 
-    _gridMenu = [[CNPGridMenu alloc] initWithMenuItems:@[_gongjijinItem, _shebaoItem, _yaohaoItem, _settingsItem]];
-    _gridMenu.delegate = self;
-    
+    _mainMenu = [[AwesomeMenu alloc] initWithFrame:self.view.bounds
+                                         startItem:_startItem
+                                         menuItems:@[_gongjijinItem, _shebaoItem, _yaohaoItem, _settingsItem]];
+    _mainMenu.delegate = self;
+    _mainMenu.startPoint = CGPointMake(DTScreenWidth/2, DTScreenHeight - 30);
+    _mainMenu.menuWholeAngle = M_PI;
+    _mainMenu.rotateAngle = -M_PI_2;
+    [self.view addSubview:_mainMenu];
 }
 
-- (void)showGridMenu:(id)sender
-{
-    [self presentGridMenu:_gridMenu animated:YES completion:^{
-        NSLog(@"Grid Menu Presented");
-    }];
-}
+#pragma mark - AwesomeMenuDelegate
 
-#pragma mark - CNPGridMenuDelegate
-
-- (void)gridMenuDidTapOnBackground:(CNPGridMenu *)menu
+- (void)awesomeMenu:(AwesomeMenu *)menu didSelectIndex:(NSInteger)idx;
 {
-    [self dismissGridMenuAnimated:YES completion:^{
-        NSLog(@"Grid Menu Dismissed With Background Tap");
-    }];
-}
-
-- (void)gridMenu:(CNPGridMenu *)menu didTapOnItem:(CNPGridMenuItem *)item
-{
-    [self dismissGridMenuAnimated:YES completion:^{
-        NSLog(@"Grid Menu Did Tap On Item: %@", item.title);
-    }];
-    
+    AwesomeMenuItem *item = menu.menuItems[idx];
     UIViewController *destVC;
     if ([item isEqual:_gongjijinItem])
     {
@@ -111,7 +103,6 @@
     {
         destVC = [[SZTGongjijinController alloc] init];
     }
-    destVC.title = item.title;
     
     UINavigationController *navigationController = [[UINavigationController alloc] initWithRootViewController:destVC];
     [self setTransitioningDelegate:[RZTransitionsManager shared]];

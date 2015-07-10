b664b4f17d155f6fab6bc7fd1f8254cb0bea1faa
iStar <istar309@gmail.com>
2015-07-10 13:00:19 +0800

    增加查询不同省份油价

--- SZTool/Gas/SZTGasPriceViewController.m
+++ SZTool/Gas/SZTGasPriceViewController.m
@@ -8,6 +8,12 @@
 
 #import "SZTGasPriceViewController.h"
 #import "BaiduAPIUtils.h"
+#import "ActionSheetStringPicker.h"
+
+#define kProvinces  @[@"北京",@"天津",@"河北",@"内蒙古",@"山西",@"上海",@"安徽",@"江苏",\
+                        @"浙江",@"山东",@"江西",@"福建",@"广东",@"广西",@"海南",@"河南",\
+                        @"湖北",@"湖南",@"黑龙江",@"吉林",@"辽宁",@"陕西",@"甘肃",@"宁夏",\
+                        @"青海",@"新疆",@"重庆",@"四川",@"云南",@"贵州",@"西藏"]
 
 static NSString *const kGas0     = @"p0";
 static NSString *const kGas90    = @"p90";
@@ -17,13 +23,18 @@ static NSString *const kProvince = @"prov";
 
 static NSString *const CellIndentifier = @"CellIndentifier";
 
+static NSString *const kDefaultProvince = @"广东";
+
 @interface SZTGasPriceViewController () <UITableViewDataSource, UITableViewDelegate>
 
 @property (nonatomic ,strong) UITableView *tableView;
+@property (nonatomic, strong) UIBarButtonItem *provBtn;
 
 @property (nonatomic, copy) NSDictionary *dataSource;
 @property (nonatomic, copy) NSArray *titles;
 @property (nonatomic, copy) NSArray *keys;
+
+@property (nonatomic, copy) NSString *currentProvince;
 
 @end
 
@@ -33,15 +44,19 @@ static NSString *const CellIndentifier = @"CellIndentifier";
     [super viewDidLoad];
 
     _titles = @[@"90号汽油", @"93号汽油", @"97号汽油", @"0号柴油"];
+    _currentProvince = kDefaultProvince;
     _keys = @[kGas90, kGas93, kGas97, kGas0];
 
     [self setupViews];
-    [self fetchData];
+    [self fetchData:nil];
 }
 
 - (void)setupViews
 {
     self.title = @"今日油价";
+    
+    _provBtn = [[UIBarButtonItem alloc] initWithTitle:kDefaultProvince style:UIBarButtonItemStylePlain target:self action:@selector(selectProvince:)];
+    self.navigationItem.rightBarButtonItem = _provBtn;
     
     _tableView = [[UITableView alloc] initWithFrame:self.view.bounds style:UITableViewStyleGrouped];
     _tableView.autoresizingMask = UIViewAutoresizingFlexibleWidth|UIViewAutoresizingFlexibleHeight;
@@ -52,15 +67,37 @@ static NSString *const CellIndentifier = @"CellIndentifier";
     [self.view addSubview:_tableView];
 }
 
-- (void)fetchData {
+- (void)selectProvince:(id)sender
+{
+    [ActionSheetStringPicker showPickerWithTitle:@"选择省份"
+                                            rows:kProvinces
+                                initialSelection:[kProvinces indexOfObject:self.currentProvince]
+                                       doneBlock:^(ActionSheetStringPicker *picker, NSInteger selectedIndex, id selectedValue) {
+                                           [self fetchData:selectedValue];
+                                       }
+                                     cancelBlock:^(ActionSheetStringPicker *picker) {
+        
+                                     }
+                                          origin:self.view];
+}
+
+- (void)fetchData:(NSString *) province {
+    NSString *prov = province;
+    if (!prov) {
+        prov = kDefaultProvince;
+    }
+    
     [self.view dt_postLoading:nil];
     WEAK_SELF;
-    [BaiduAPIUtils fetchGasPriceOfProvinc:@"广东" doneBlock:^(NSDictionary *result, NSError *error) {
+    [BaiduAPIUtils fetchGasPriceOfProvinc:prov doneBlock:^(NSDictionary *result, NSError *error) {
         STRONG_SELF_AND_RETURN_IF_SELF_NULL;
         [self.view dt_cleanUp:YES];
         if (error == nil && result.allKeys.count > 1) {
             self.dataSource = result;
             [self.tableView reloadData];
+            
+            self.currentProvince = prov;
+            self.provBtn.title = self.currentProvince;
         }
         else {
             [self.view dt_postError:error.localizedDescription];
